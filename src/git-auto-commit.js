bd70e31b57bbe4513843dc1a7eaea31f609dbcac
Jack Witbrock Finlay <jack@jackfinlay.com>
2016-08-06 11:56:51 +1200

    Change name of file.

---
+++ src/git-auto-commit.js
@@ -0,0 +1,23 @@
+var exec     = require('child_process').exec;
+var chokidar = require('chokidar');
+
+var watcher = chokidar.watch('.', {ignored: /[\/\\]\.[\.git]/});
+
+console.log(watcher.getWatched());
+
+watcher.on('change', function(path) { onChange(path);});
+
+var onAdd = function (path) {
+
+	exec('git add ${path}');
+	console.log('File ' + path +' added to git repo.');
+	onChange(path);
+};
+
+var onChange = function (path) {
+	var timestamp = new Date().toString();
+
+	exec('git commit -am \"Committed by git-auto-commit @ ' + timestamp + '\"');
+	console.log('File ' + path +' commited to the git repo.');
+};
+
