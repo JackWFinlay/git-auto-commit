f1a318a34387d92831e4cdbcd5cd9763fc861c9d
Jack Witbrock Finlay <jack@jackfinlay.com>
2016-08-06 12:03:10 +1200

    Committed by git-auto-commit @ Sat Aug 06 2016 12:03:10 GMT+1200 (NZST)

--- src/git-auto-commit.js
+++ src/git-auto-commit.js
@@ -1,7 +1,7 @@
 var exec     = require('child_process').exec;
 var chokidar = require('chokidar');
 
-var watcher = chokidar.watch('.', {ignored: /[\/\\]\.[\.git]/});
+var watcher = chokidar.watch(process.argv[1], {ignored: /[\/\\]\./});
 
 console.log(watcher.getWatched());
 
@@ -20,4 +20,3 @@ var onChange = function (path) {
 	exec('git commit -am \"Committed by git-auto-commit @ ' + timestamp + '\"');
 	console.log('File ' + path +' commited to the git repo.');
 };
-
